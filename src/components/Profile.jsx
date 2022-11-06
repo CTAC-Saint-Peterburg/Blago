5c1c57edf4636b8422746cfdafa3627b57f41d4e
Стас <seleznev-00@mail.ru>
2022-11-06 17:24:50 +0300

    update: styles.modules

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,15 @@ import styles from "../stylesComponents/Profile.module.css";
 export default function Profile() {
   return (
     <div className={styles.profile}>
-      <h1>Profile</h1>
+      <div className={styles.divprofile}>
+        <h1 className={styles.profileh1}>Balance</h1>
+        <h2 className={styles.profileh2}>1002¥</h2>
+        <button>Пополнить</button>
+        <button>Снять</button>
+      </div>
+      <div className={styles.divprofile}>
+        <h1 className={styles.profileh1}>Profile</h1>
+      </div>
     </div>
   );
 }
