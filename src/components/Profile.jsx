726023454e62c64bca32d249a46d6e8c87b6aab3
Стас <seleznev-00@mail.ru>
2022-11-07 18:58:01 +0300

    update: profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -12,6 +12,22 @@ export default function Profile() {
       </div>
       <div className={styles.divprofile}>
         <h1 className={styles.profileh1}>Profile</h1>
+        <div className={styles.divprofileoptions}>
+          <div>asd</div>
+          <div className={styles.divimg}>
+            <img
+              className={styles.imgprofile}
+              src={require("../images/stock.jpg")}
+              alt="image"
+            />
+            <h2 className={styles.h2imgprofile}>Username</h2>
+          </div>
+          <div className={styles.settingsimgprofile}>
+            <div className={styles.settingsdiv}></div>
+            <div className={styles.settingsdiv}></div>
+            <div className={styles.settingsdiv}></div>
+          </div>
+        </div>
       </div>
     </div>
   );
