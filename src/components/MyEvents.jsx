1e2d7e1a1d58c0619d13bc25cc46b8721b2ff4ec
Стас <seleznev-00@mail.ru>
2022-11-13 18:02:20 +0300

    init:my events

--- src/components/MyEvents.jsx
+++ src/components/MyEvents.jsx
@@ -1,8 +1,18 @@
 import styles from "../stylesComponents/MyEvents.module.css";
+import CoinFlipCard from "./secundaryComponents/Coinflipcard";
 export default function MyEvents() {
   return (
     <div className={styles.myevents}>
-      <h1>MyEvents</h1>
+      <div className={styles.myeventsheader}>
+        <h1 className={styles.myeventsh1}>My events</h1>
+      </div>
+      <div className={styles.myeventsalldata}>
+        <CoinFlipCard />
+        <CoinFlipCard />
+        <CoinFlipCard />
+        <CoinFlipCard />
+        <CoinFlipCard />
+      </div>
     </div>
   );
 }
---
+++ src/components/secundaryComponents/Coinflipcard.jsx
@@ -0,0 +1,33 @@
+import styles from "./Coinflipcard.module.css";
+export default function CoinFlipCard(props) {
+  return (
+    <>
+      <div className={styles.cardMain}>
+        <div className={styles.cardProfiles}>
+          <div>
+            <img
+              className={styles.cardProfilesImg}
+              src={require("./../../images/stock.jpg")}
+              alt="profile"
+            />
+          </div>
+          <div>
+            <h1 className={styles.cardProfilesH1}>VS</h1>
+          </div>
+          <div>
+            <img
+              className={styles.cardProfilesImg}
+              src={require("./../../images/stock.jpg")}
+              alt="profile"
+            />
+          </div>
+        </div>
+        <div>событие</div>
+        <div>итог</div>
+        <div>
+          <button className={styles.cardButtonMore}>подробнее</button>
+        </div>
+      </div>
+    </>
+  );
+}
