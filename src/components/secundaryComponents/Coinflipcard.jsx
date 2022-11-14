c9df9dc5b06c92d82a30414378a3acbe9d51b086
Стас <seleznev-00@mail.ru>
2022-11-14 18:34:44 +0300

    update: My events

--- src/components/secundaryComponents/Coinflipcard.jsx
+++ src/components/secundaryComponents/Coinflipcard.jsx
@@ -22,8 +22,21 @@ export default function CoinFlipCard(props) {
             />
           </div>
         </div>
-        <div>событие</div>
-        <div>итог</div>
+        <div className={styles.cardEvent}>
+          <h3 className={styles.cardEventH3}>cобытие:</h3>
+          <h2 className={styles.cardEventH2}>Чемпионат мира 2022</h2>
+        </div>
+        <div className={styles.cardResult}>
+          <h3 className={styles.cardResulth3}>итог:</h3>
+          <p className={styles.cardResultP}>winner</p>
+          <div>
+            <img
+              className={styles.cardResultImg}
+              src={require("./../../images/stock.jpg")}
+              alt=""
+            />
+          </div>
+        </div>
         <div>
           <button className={styles.cardButtonMore}>подробнее</button>
         </div>
