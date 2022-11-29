213d3b23c61dd734b2384e236d1f0dd6f18f0df7
Стас <seleznev-00@mail.ru>
2022-11-29 18:04:17 +0300

    update: Modal style

--- src/components/ModalComponens/ModalCardMore.jsx
+++ src/components/ModalComponens/ModalCardMore.jsx
@@ -1,9 +1,61 @@
+import { useState } from "react";
+import style from "./ModalCardMore.module.css";
 export default function ModalCardMore(props) {
+  const [datadefault, setDatadefault] = useState({
+    title: "карточка: 678",
+    playerOne: { name: "kek", status: "win" },
+    playerTwo: { name: "kok", status: "lose" },
+    event: "Чемпионат Мира",
+  });
   return (
     <>
-      <div>
-        <div>
-          <h1>Test</h1>
+      <div className={style.main}>
+        <h1 className={style.mainTitle}>{datadefault.title}</h1>
+        <div className={style.cardInfoMain}>
+          <div className={style.profileBlock}>
+            <img
+              className={style.img}
+              src={require("./../../images/stock.jpg")}
+              alt=""
+            />
+            <h2>{datadefault.playerOne.name}</h2>
+            <div className={style.profileStatus}>
+              <h3
+                className={
+                  datadefault.playerOne.status === "win"
+                    ? `${style.win}`
+                    : `${style.lose}`
+                }
+              >
+                {datadefault.playerOne.status}
+              </h3>
+            </div>
+          </div>
+
+          <div>
+            <h2 className={style.vs}>VS</h2>
+            <h4 className={style.event}>{datadefault.event}</h4>
+          </div>
+          <div className={style.profileBlock}>
+            <img
+              className={style.img}
+              src={require("./../../images/stock.jpg")}
+              alt=""
+            />
+            <h2>{datadefault.playerTwo.name}</h2>
+            <div className={style.profileStatus}>
+              <h3
+                className={
+                  datadefault.playerTwo.status === "win"
+                    ? `${style.win}`
+                    : `${style.lose}`
+                }
+              >
+                {" "}
+                {datadefault.playerTwo.status}
+              </h3>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/components/MyEvents.jsx
+++ src/components/MyEvents.jsx
@@ -4,11 +4,10 @@ import { useSelector } from "react-redux";
 import ModalCardMore from "./ModalComponens/ModalCardMore";
 export default function MyEvents() {
   const usersList = useSelector((state) => state.users.usersList);
-  const display = useSelector((state) => state.modal.modalList);
-  console.log(display);
+  const displayMyEventsModal = useSelector((state) => state.modal.modalList);
   return (
     <div className={styles.myevents}>
-      {display.show && <ModalCardMore />}
+      {displayMyEventsModal.show && <ModalCardMore />}
       <div className={styles.myeventsheader}>
         <h1 className={styles.myeventsh1}>My events</h1>
       </div>
